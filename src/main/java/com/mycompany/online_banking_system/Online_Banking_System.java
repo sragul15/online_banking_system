fc7af9bc40bfe4e860b8f4c1d08ce5a29929e394
sragul15 <raguls0115@gmail.com>
2024-09-27 23:09:40 +0530

    initial commit

---
+++ src/main/java/com/mycompany/online_banking_system/Online_Banking_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.online_banking_system;
+
+/**
+ *
+ * @author 91636
+ */
+public class Online_Banking_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
